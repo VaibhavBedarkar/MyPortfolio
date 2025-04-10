845aa34cd0a68a729c7d4dd5e5691d26a1d105bb
vaibhavbedarkar <vaibhav.bedarkar98@gmail.com>
2025-04-10 16:19:51 +0530

    [Deployment]: CI/CD Integration

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <>
   
       <h1>Vaibhav Bedarkar</h1>
+      <h2>CI/CD Deployment</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
